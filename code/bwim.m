76d2fd9dd66a3263c3ed362efed7f1c96b0ab627
torholmslettebak <torholmslettebak@gmail.com>
2016-02-01 16:26:00 +0100

    Updated code, now it draws and alculates influence lines Atm only for two sensors, needs a control.

--- code/bwim.m
+++ code/bwim.m
@@ -1,6 +1,6 @@
-
+clf
 % Lengt of bridge [m]
-L = 15;
+L = 30;
 % Number of sensors
 n = 2;
 % Distance between sensors
@@ -8,19 +8,33 @@ n_d = 5;
 % Distane from reaction A to first sensor
 L_a = 5;
 % Distance from reaction A to furthest sensor
-L_b = 10;
+L_b = 25;
+
+clf
 
 
-
-
-function generateInfluenceLines(L, n, n_d, L_a, L_b)
-numberOfLines = n*2;
-% generate functions for each line
-% i = 1:numberOfLines;
-maxA = L_a*(1-L_a/L);
-maxB = L_b*(1-L_b/L);
-line([0 0],[5 maxA]);
-line([5 maxA],[15 0]);
+function [a,b, c, d] =  generateInfluenceLine(L, L_a)
+	magnitude = L_a*(1- L_a / L);
+	a = magnitude/L_a;
+	b=0;
+	c = -magnitude/(L-L_a);
+	d = magnitude;
 end
 
-generateInfluenceLines(L, n, n_d, L_a, L_b)
+function yValue =  drawInfluenceLine(a, b, c, d, L_a, L)
+	x1 = 0:0.01:L_a;
+	x2 = L_a:0.01:L;
+	y1 = a*x1 + b;
+	y2 = c*(x2-L_a) + d;
+	y2(1) = [];
+	x2(1) = [];
+	x = [x1, x2];
+	y = [y1,y2];
+	yValue = y;
+	plot(x,y)
+end
+[a,b,c,d] = generateInfluenceLine(L, L_a)
+sensor1Vector = drawInfluenceLine(a,b,c,d, L_a, L);
+hold on
+[a,b,c,d] = generateInfluenceLine(L, L_b)
+sensor2Vector = drawInfluenceLine(a,b,c,d, L_b, L);
