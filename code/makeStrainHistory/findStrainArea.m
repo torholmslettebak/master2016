c4a448bd720be0ad63392ae904f66347e71f301e
torholmslettebak <torholmslettebak@gmail.com>
2016-04-04 21:59:40 +0200

    Big update of readStrain  and findStrainArea.. it now gathers the essential part of the strain history.. That means the actual train passing is ready for testing..
    There seems to be a problem with the axle detection function, it finds to many peaks in the function.. that may be due to changes in the filtering method leading to to little being cut off through the butter filter..  This Should be possible to remove by adapting the minimum peak height in the axle detection function..

--- code/makeStrainHistory/findStrainArea.m
+++ code/makeStrainHistory/findStrainArea.m
@@ -1,7 +1,64 @@
-function [ strain ] = findStrainArea( raw_strain )
+function [ strain, beginning, ending ] = findStrainArea( raw_strain, t )
 %FINDSTRAINAREA Supposed to remove all unessential parts of the strain
-%history.
+%history. Will also set initial y value to zero..
+addpath('filtering');
+filteredStrain = denoiseSignal(raw_strain, 20);
+d1 = diff(filteredStrain);
+temp = d1;
+temp(temp<0) = 0;
+filteredPositive = temp;
+test = mean(filteredPositive);
+[pks, locs] = findpeaks(filteredPositive, 'MinPeakHeight',mean(filteredPositive)*3);
+temp = d1;
+temp(temp>0) = 0;
+filteredNegative = temp;
+test2 = mean(filteredNegative);
+[pks2, locs2] = findpeaks(-filteredNegative, 'MinPeakHeight',-mean(filteredNegative)*3);
+peakLocs = [locs ;locs2];
+minLoc = min(peakLocs);
 
+figure(2)
+plot(t(1:length(d1)), d1);
+% legend('selected', 'whole');
+
+% Finding the next time line passes or equals zero
+i = minLoc;
+zeroNotFound = 1;
+while zeroNotFound == 1
+    if d1(minLoc) > 0 && (d1(i) <= 0)
+        beginning = i;
+        zeroNotFound = 0;
+    elseif d1(minLoc) < 0 && (d1(i) >= 0)
+        beginning = i;
+        zeroNotFound = 0;
+    else
+        i = i-1;    
+    end
+end
+% Finding the next time line passes or equals zero
+maxLoc = max(peakLocs);
+i = maxLoc;
+zeroNotFound = 1;
+while zeroNotFound==1
+    if d1(maxLoc) > 0 && (d1(i) <= 0)
+        ending = i;
+        zeroNotFound = 0;
+    elseif d1(maxLoc) < 0 && (d1(i) >= 0)
+        ending = i;
+        zeroNotFound = 0;
+    else
+        i = i+1;    
+    end
+end
+% figure(2)
+% plot(t(beginning:ending), d1(beginning:ending), '.', t(1:length(d1)), d1);
+% legend('selected', 'whole');
+
+% account for locations obtained by derivatives.. diff reduces number of
+% indexes by 1
+beginning = beginning + 1;
+ending = ending + 1;
+strain = raw_strain(beginning:ending);
 
 end
 
--- code/makeStrainHistory/readStrain.m
+++ code/makeStrainHistory/readStrain.m
@@ -13,40 +13,19 @@ s1 = M(:,2);
 s2 = M(:,3);
 s3 = M(:,4);
 delta_t = t(2)-t(1);
-freq = findNoiseFrequency(s1(1:100), delta_t)
+freq = findNoiseFrequency(s1(1:100), delta_t);
 denoisedS1 = denoiseSignal(s1, freq*1000);
-diff1 = diff((denoisedS1));
-mean(denoisedS1)
-% [pks, locs, w, p] = findpeaks(diff1, 'MinPeakHeight',mean(diff1)*100);
-% test = denoisedS1(locs(1):locs(length(locs)));
+[s1fixed, beginning1, ending1] = findStrainArea(s1, t);
+disp('done1')
+[s2fixed, beginning2, ending2] = findStrainArea(s2, t);
+disp('done2')
+[s3fixed, beginning3, ending3] = findStrainArea(s3, t);
+disp('done3')
 figure(1)
-% plot(t, denoisedS1, t, s2, t, s3, t(locs(1):locs(length(locs))), test);
-plot(t, s1, t, s2, t, s3)
-legend('Sensor1', 'Sensor2','Sensor3');
-% figure(2)
-% plot(t(1:length(t)-1), diff1);
-% for i = 2:length(files)
-%    load(files(i).name)
-% end
-% filename = files(2).name;
-% fileID = fopen(files(1).name, 'r');
-% C = textscan(fileID, '%f', '%f', '%f', '%f');
-% C = textscan(fileID,'%f',4,'Delimiter','');
-% fclose(fileID);
-% files(2).name
-% fileID = fopen(files(2).name, 'r');
+plot(t, s1, t, s2, t, s3, t(beginning1:ending1), s1fixed, '.', t(beginning2:ending2), s2fixed, '.', t(beginning3:ending3), s3fixed, '.')
+legend('Sensor1', 'Sensor2','Sensor3', 'cutout');
 
-% filename = files(2).name;
-% M = dlmread(filename,' ',2,0)
-% C = textscan(fileID, '%f', 'Delimiter', ' ');
-% for i=1:length(files)
-%     
-%     tline = fgets(fileID);
-%     tline = fgets(files(i).name);
-%     eval(['load ' files(i).name ' -ascii']);
-% end
-% fileID = fopen('1603161026.txt','r');
-% tline = fgets(fileID);
+
 
 end
 
